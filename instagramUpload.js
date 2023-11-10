38bfbf65b553cef04a3aa1ebb8c51edd7d8991c6
Lleir Garcia Boada <lleir.garcia@securitasdirect.es>
2023-11-10 14:02:59 +0100

    Add images to repo.

--- instagramUpload.js
+++ instagramUpload.js
@@ -48,7 +48,7 @@ async function main() {
         for (const tema of posts) {
             for (const subtema of tema.subtemas) {
                 for (const post of subtema.posts) {
-                    const imageUrl = `https://raw.githubusercontent.com/lleirgarcia/automata/main/imagenes/${post.id}.jpg`; // Asumiendo que la imagen tiene el mismo ID que el post y es un archivo PNG
+                    const imageUrl = `https://raw.githubusercontent.com/lleirgarcia/automata/develop/imagenes/${post.id}.jpg`; // Asumiendo que la imagen tiene el mismo ID que el post y es un archivo PNG
                     console.log("Image")
                     console.log(imageUrl)
                     const containerId = await createContainer(imageUrl, post.content);
