c1035573d785f68593b8e88660fde23a8eca044f
Lleir Garcia Boada <lleir.garcia@securitasdirect.es>
2023-11-12 23:59:46 +0100

    auto adding images to repo.

--- createPostV2.js
+++ createPostV2.js
@@ -31,7 +31,11 @@ async function generateStory(theme, subtheme) {
     const prompt = `Dentro de la materia general "${theme}" y más en específico de "${subtheme}" eres un experto en el tema 
                     y necesito 1 posts para Instagram, de entre 3-5 pasos que una persona debería de hacer para acometer esos puntos y que le sirva como guía 
                     y que sean distintos a los posts que escribiste anteriormente sobre el mismo tema.
-                    Recuerda que son posts para instagram, así que ponlos bonitos. Por favor, no me añadas imagenes tipo. Escribemelo en ingles.`;
+                    Recuerda que son posts para instagram, así que ponlos bonitos. Por favor, no me añadas imagenes tipo. 
+                    Escribemelo en ingles.
+                    En tu contestacion, escribeme unicamente los pasos o la descripcion que añadirias al post, nada de "Sure, I'd be happy to assist. Here's a suggestion for a post" ni enumeracion de posts ni nada por el estilo, solamente el texto que copiaré y pegaré para su uso.
+                    En el caso de que enumeres puntos (1, 2, 3), utiliza los emojis de watsapp.
+                    Dejalo limpio de (**).`;
 
     try {
         const chatCompletion = await openai.chat.completions.create({
@@ -57,17 +61,14 @@ async function generatePostsWithTextAndImages(qttPosts, filePath) {
         var result = [];
 
         console.log("...Reading JSON to create posts...")
-        console.log("Temas: " + data)
 
         for (const tema of temas) {
-            console.log("Tema: " + tema.nombre)
             let temaObject = {
                 tema: tema.nombre,
                 subtemas: []
             };
 
             for (const subtema of tema.subtemas) {
-                console.log("Subtema: " + subtema)
                 let subtemaObject = {
                     subtema: subtema,
                     posts: []
