806780e4ae9a94deb19bc67eba6b8c7fac30ba86
automatic_upload <lleirgarcia@gmail.com>
2023-12-12 21:05:33 +0100

    auto adding images to repo.

--- createPostV2.js
+++ createPostV2.js
@@ -126,55 +126,54 @@ function removeSubtema(temaPrincipal, subtemaNombre) {
     // Filtra los subtemas para excluir el que coincide con subtemaNombre
     tema.subtemas = tema.subtemas.filter(st => st.nombre !== subtemaNombre);
     fs.writeFileSync("./topicsandsubtopics.json", JSON.stringify(temas, null, 2));
-
 }
 
 
+// /**
+//  * Construye el objeto de resultados basado en los temas y subtemas.
+//  * @returns {Promise<void>}
+//  */
+// async function generatePostsWithTextAndImages(temaSubtema) {
+//     console.log("dasdasdasdad")
+//     try {
+//         var result = [];
+//         const partes = temaSubtema.split(',');
 
-/**
- * Construye el objeto de resultados basado en los temas y subtemas.
- * @returns {Promise<void>}
- */
-async function generatePostsWithTextAndImages(temaSubtema) {
-    try {
-        var result = [];
-        const partes = temaSubtema.split(',');
+//         var tema = partes[0];
+//         var subtema = partes[1];
+//         var color = partes[2];
 
-        var tema = partes[0];
-        var subtema = partes[1];
-        var color = partes[2];
+//         console.log("...Reading topic...")
+//         console.log(`Tema ${tema}, Subtema ${subtema}, Color ${color}`)
 
-        console.log("...Reading topic...")
-        console.log(`Tema ${tema}, Subtema ${subtema}, Color ${color}`)
-
-        let temaObject = {
-            tema: tema,
-            subtemas: []
-        };
+//         let temaObject = {
+//             tema: tema,
+//             subtemas: []
+//         };
             
-        let subtemaObject = {
-            subtema: subtema,
-            posts: []
-        };
-        let idrandom;
+//         let subtemaObject = {
+//             subtema: subtema,
+//             posts: []
+//         };
+//         let idrandom;
                 
-        idrandom = generateRandomString(10);
-        const story = await generateStory(tema, subtema);
-        const post = {
-            id: idrandom, // Genera un ID aleatorio para el post
-            content: story
-        };
-        generateImagesFromPrompt(idrandom, story, color);
-        subtemaObject.posts.push(post);      
-        temaObject.subtemas.push(subtemaObject);
-        result.push(temaObject);
+//         idrandom = generateRandomString(10);
+//         const story = await generateStory(tema, subtema);
+//         const post = {
+//             id: idrandom, // Genera un ID aleatorio para el post
+//             content: story
+//         };
+//         generateImagesFromPrompt(idrandom, story, color);
+//         subtemaObject.posts.push(post);      
+//         temaObject.subtemas.push(subtemaObject);
+//         result.push(temaObject);
         
     
-        await fs.writeFile('temasConHistoriasV2.json', JSON.stringify(result, null, 2), 'utf8');
-    } catch (error) {
-        console.error("Error al leer o procesar el archivo JSON:", error);
-    }
-}
+//         await fs.writeFile('temasConHistoriasV2.json', JSON.stringify(result, null, 2), 'utf8');
+//     } catch (error) {
+//         console.error("Error al leer o procesar el archivo JSON:", error);
+//     }
+// }
 
 // Función principal que se ejecuta al iniciar el script
 async function main(filePath, postNumberBySubTopic, temaSubtema) {
@@ -183,10 +182,15 @@ async function main(filePath, postNumberBySubTopic, temaSubtema) {
     console.log("args  posts: " + postNumberBySubTopic)
     console.log("args temas subtema: " + temaSubtema)
 
-    if(temaSubtema)
-        await generatePostsWithTextAndImages(temaSubtema);
-    else
+    let json = filePath != undefined && temaSubtema == undefined
+    console.log("is: " + json)
+    if(json) {
         await generatePostsWithTextAndImages(postNumberBySubTopic, filePath);
+    } 
+    // else {
+    //     await generatePostsWithTextAndImages(temaSubtema);
+    // }
+    
 }
 
 const jsonFilePath = process.argv[2] || 'topicsandsubtopics.json';
